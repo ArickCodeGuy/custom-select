9a4f63e10950ae7b26f8278a097191394294c70d
unknown <arickmail1997@gmail.com>
2021-05-11 11:16:05 +0300

    specified private methods

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const defaultOptions = {
 class CustomSelect {
   constructor(el, options) {
     this.el = el;
-    this.elType = this.getType();
+    this.elType = this.#getType();
     this.options = Object.assign({}, defaultOptions, options);
     this.options.init ? this.init(): false;
   }
 
-  createOptions(elObj) {
+  #createOptions(elObj) {
     let optionsEl = document.createElement('div');
     optionsEl.classList.add('custom-select-options');
     optionsEl.style.display = 'none';
@@ -31,14 +31,14 @@ class CustomSelect {
     return optionsEl
   }
 
-  createPlaceholder(elObj) {
+  #createPlaceholder(elObj) {
     let placeholder = document.createElement('div');
     placeholder.innerHTML = elObj.selectEl.querySelector(`option:nth-child(${elObj.selectEl.selectedIndex + 1})`).innerHTML;
     placeholder.classList.add('custom-select-placeholder');
     return placeholder
   }
 
-  createCustomSelect(elObj) {
+  #createCustomSelect(elObj) {
     let customSelectEl = document.createElement('div');
     customSelectEl.classList.add('custom-select-select');
     customSelectEl.insertAdjacentElement('afterbegin', elObj.placeholder);
@@ -46,11 +46,11 @@ class CustomSelect {
     return customSelectEl
   }
 
-  create(elObj) {
+  #create(elObj) {
     // Creating custom select elements
-    elObj.customOptionsEl = this.createOptions(elObj);
-    elObj.placeholder = this.createPlaceholder(elObj);
-    elObj.customSelectEl = this.createCustomSelect(elObj);
+    elObj.customOptionsEl = this.#createOptions(elObj);
+    elObj.placeholder = this.#createPlaceholder(elObj);
+    elObj.customSelectEl = this.#createCustomSelect(elObj);
 
     // hide / not hide <select> element if option is specified
     this.options.hideSelect ? elObj.selectEl.style.display = 'none': false;
@@ -59,16 +59,16 @@ class CustomSelect {
     elObj.el.insertAdjacentElement('beforeend', elObj.customSelectEl);
   }
 
-  addEvents(elObj) {
+  #addEvents(elObj) {
     // click on placeholder
-    elObj.placeholder.addEventListener('click', () => this.toggleOptions(elObj));
+    elObj.placeholder.addEventListener('click', () => this.#toggleOptions(elObj));
 
     // click on custom option element
     elObj.customOptionElArr.forEach((customOptionElement) => {
       customOptionElement.addEventListener('click', (e) => {
         if (!customOptionElement.classList.contains('disabled')) {
           elObj.placeholder.innerHTML = customOptionElement.innerHTML;
-          this.toggleOptions(elObj);
+          this.#toggleOptions(elObj);
           elObj.selectEl.value = customOptionElement.dataset.value;
 
           elObj.customOptionsEl.querySelector('.selected').classList.remove('selected');
@@ -82,29 +82,29 @@ class CustomSelect {
       // currently there is no removeEventListener for this event on destroy()
       document.addEventListener('click', (e) => {
         if (!e.target === elObj.el || !elObj.el.contains(e.target)) {
-          this.closeOptions(elObj);
+          this.#closeOptions(elObj);
         };
       });
     };
   }
 
-  toggleOptions(elObj) {
+  #toggleOptions(elObj) {
     elObj.el.classList.toggle('options-toggled');
     elObj.customOptionsEl.classList.toggle('toggled');
     elObj.customOptionsEl.style.display === 'none' ? elObj.customOptionsEl.style.display = 'block': elObj.customOptionsEl.style.display = 'none';
   }
 
-  closeOptions(elObj) {
+  #closeOptions(elObj) {
     elObj.el.classList.remove('options-toggled');
     elObj.customOptionsEl.classList.remove('toggled');
     elObj.customOptionsEl.style.display = 'none';
   }
 
-  afterInitFunc(elObj) {
+  #afterInitFunc(elObj) {
     typeof this.options.afterInit === 'function' ? this.options.afterInit(): false;
   }
 
-  initSingleElement(el) {
+  #initSingleElement(el) {
     const elObj = {
       el: el,
       selectEl: el.querySelector('select'),
@@ -119,28 +119,17 @@ class CustomSelect {
     };
     elObj.optionElArr = elObj.selectEl.querySelectorAll('option');
 
-    this.create(elObj);
-    this.addEvents(elObj);
-    this.afterInitFunc(elObj);
+    this.#create(elObj);
+    this.#addEvents(elObj);
+    this.#afterInitFunc(elObj);
     el.classList.add('custom-select-initialized');
   }
 
-  initArray(arr) {
-    arr.forEach((el, i) => this.initSingleElement(el));
+  #initArray(arr) {
+    arr.forEach((el, i) => this.#initSingleElement(el));
   }
 
-  init() {
-    if (this.elType === 'string') {
-      const el = document.querySelectorAll(this.el);
-      el ? this.initArray(el): false;
-    }else if (this.elType === 'NodeList') {
-      this.initArray(this.el);
-    }else if (this.elType === 'HTMLElement') {
-      this.initSingleElement(this.el);
-    };
-  }
-
-  destroySingleElement(el) {
+  #destroySingleElement(el) {
     let customSelect = el.querySelector('.custom-select-select');
     customSelect.remove();
     el.classList.remove('custom-select-initialized');
@@ -148,8 +137,31 @@ class CustomSelect {
     el.querySelector('select').style.display = 'block';
   }
 
-  destroyArray(arr) {
-    arr.forEach((el) => this.destroySingleElement(el));
+  #destroyArray(arr) {
+    arr.forEach((el) => this.#destroySingleElement(el));
+  }
+
+  #getType() {
+    if (typeof this.el === 'string') {
+      return 'string'
+    }else if (NodeList.prototype.isPrototypeOf(this.el)) {
+      return 'NodeList'
+    }else if (HTMLElement.prototype.isPrototypeOf(this.el)) {
+      return 'HTMLElement'
+    }else {
+      throw `CustomSelect err: specified el: ${this.el} doesn't match eny allowed type of variable`;
+    };
+  }
+
+  init() {
+    if (this.elType === 'string') {
+      const el = document.querySelectorAll(this.el);
+      el ? this.#initArray(el): false;
+    }else if (this.elType === 'NodeList') {
+      this.#initArray(this.el);
+    }else if (this.elType === 'HTMLElement') {
+      this.#initSingleElement(this.el);
+    };
   }
 
   destroy() {
@@ -160,18 +172,6 @@ class CustomSelect {
       this.destroyArray(this.el);
     }else if (this.elType === 'HTMLElement') {
       this.destroySingleElement(this.el);
-    };
-  }
-
-  getType() {
-    if (typeof this.el === 'string') {
-      return 'string'
-    }else if (NodeList.prototype.isPrototypeOf(this.el)) {
-      return 'NodeList'
-    }else if (HTMLElement.prototype.isPrototypeOf(this.el)) {
-      return 'HTMLElement'
-    }else {
-      throw `CustomSelect err: specified el: ${this.el} doesn't match eny allowed type of variable`;
     };
   }
 };
